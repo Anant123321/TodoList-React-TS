fa3da333038bdef49ba22c2fb488c2b24075c96e
Anant <eranant.shibe@gmail.com>
2024-01-28 17:34:29 +0530

    Pending and comleted section added

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,4 +1,4 @@
-import { COMPLETED } from '../../constants'
+import { COMPLETED, PENDING } from '../../constants'
 import { TodoType } from '../../types/Todo'
 import TodoCard from '../TodoCard'
 import './index.css'
@@ -35,19 +35,42 @@ const TodosList: React.FC<Props> = ({ todoList, setTodoList }) => {
     })
     setTodoList(updatedList)
   }
+  const pendingTodos: TodoType[] = todoList.filter((todo) => {
+    return todo.status === PENDING
+  })
+  const completedTodos: TodoType[] = todoList.filter((todo) => {
+    return todo.status === COMPLETED
+  })
   return (
-    <div className="todo-list">
-      {todoList.map((todo) => {
-        return (
-          <TodoCard
-            key={todo.id}
-            todo={todo}
-            deleteTodo={deleteTodo}
-            completeTodo={completeTodo}
-            updateTodo={updateTodo}
-          />
-        )
-      })}
+    <div className="todo-list-container">
+      <div className="todo-list-pending">
+        <p className="task-title">Active tasks</p>
+        {pendingTodos.map((todo) => {
+          return (
+            <TodoCard
+              key={todo.id}
+              todo={todo}
+              deleteTodo={deleteTodo}
+              completeTodo={completeTodo}
+              updateTodo={updateTodo}
+            />
+          )
+        })}
+      </div>
+      <div className="todo-list-completed">
+        <p className="task-title">Completed tasks</p>
+        {completedTodos.map((todo) => {
+          return (
+            <TodoCard
+              key={todo.id}
+              todo={todo}
+              deleteTodo={deleteTodo}
+              completeTodo={completeTodo}
+              updateTodo={updateTodo}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
